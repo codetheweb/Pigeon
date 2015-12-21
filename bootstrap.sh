df3e51c6c000e1202c366dfe6207e30bce536e38
Max Isom <codetheweb@icloud.com>
2015-12-21 11:17:06 -0600

    Add Git clonning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install apache2 php5 -y
+sudo apt-get install apache2 php5 git -y
 sudo mkdir /grip-config
 sudo chown www-data /grip-config
+cd /var/www/html
+git clone git@github.com:codetheweb/Pigeon.git
